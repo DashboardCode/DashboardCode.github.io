a1816f74cc2e18ad4ca741c2046ef223fc861b9b
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2021-09-03 01:06:24 +0300

    upd popper

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+import { createPopper } from "@popperjs/core";
+import { ModuleFactory as bsMultiSelectModuleFactory } from "@dashboardcode/bsmultiselect";
+
+const multiSelectEnv = {window, createPopper};
+const multiSelect = bsMultiSelectModuleFactory(multiSelectEnv).BsMultiSelect;
+
+$(() => {
+  $("select[multiple]").each((_i, select) => {
+    multiSelect(select);
+  });
+});
+
 // test
-import 'normalize.css/normalize.css';
+//import 'normalize.css/normalize.css';
 
 console.log("index")
--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,133 @@
-﻿// webpack.config interpretated by node and node by default do not support ES6 (that means import etc.)
-//const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-//const ManifestPlugin = require('webpack-manifest-plugin');
-//const PathModule = require('path');
+﻿/* eslint-disable no-ternary, no-process-env */
 
-// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const PostCSSPresetEnv = require("postcss-preset-env");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const isDev = process.env.NODE_ENV !== "production";
 
-// const outputFolderPath = PathModule.resolve(__dirname, 'wwwroot/dist');
+const esLintOptions = {
+  extensions: ["js"],
+  exclude: [
+    "node_modules",
+    "./config",
+  ],
+}
 
-
-var config = {
-    entry: {
-        index: './src/index.js',
-        about: './src/about.js', 
-        contacts: './src/contacts.js'
+module.exports = {
+  mode: isDev
+    ? "development"
+    : "production",
+  stats: {
+    colors: true,
+    preset: "minimal",
+  },
+  ignoreWarnings: [
+    {
+      file: /bootstrap/,
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: true,
-            chunks: ['index'],
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/about.html',
-            inject: true,
-            chunks: ['about'],
-            filename: 'about.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/contacts.html',
-            inject: true,
-            chunks: ['contacts'],
-            filename: 'contacts.html'
-        })
+  ],
+  performance: {
+    hints: isDev
+      ? false
+      : "warning",
+  },
+  devtool: isDev
+    ? "cheap-module-source-map"
+    : "source-map",
+  entry: [
+    path.resolve(__dirname, "src/scripts/main.js"),
+    path.resolve(__dirname, "src/styles/main.scss"),
+  ],
+  output: {
+    publicPath: "/assets/",
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: isDev
+      ? "[name].js"
+      : "[name].[contenthash].js",
+  },
+  plugins: [
+    new ESLintPlugin(esLintOptions),
+    new WebpackManifestPlugin(),
+    new StylelintPlugin({
+      files: "src/styles/**/*.scss",
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDev
+        ? "[name].css"
+        : "[name].[contenthash].css",
+    }),
+  ],
+  ...(!isDev && {
+    optimization: {
+      minimizer: [
+        (compiler) => {
+          Reflect.apply(
+            new TerserPlugin({
+              terserOptions: {
+                compress: {},
+              },
+            }),
+            compiler
+          );
+        },
+        new CssMinimizerPlugin(),
+      ],
+    },
+  }),
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.scss/,
+        loader: "import-glob-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s?css/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: { postcssOptions: { plugins: [PostCSSPresetEnv] } },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: `images/${isDev
+            ? "[name][ext]"
+            : "[contenthash][ext]"}`,
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: `fonts/${isDev
+            ? "[name][ext]"
+            : "[contenthash][ext]"}`,
+        },
+      },
     ],
-    module: {
-        rules: [
-           {
-                test: /\.css$/,
-                use: [
-                   "style-loader",
-                   "css-loader"
-                ]
-           },
-           { 
-                test: /\.(html)$/,
-                //include: path.join(__dirname, 'src/views'),
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                       interpolate: true
-                    }
-                }
-            }
-        ]
+  },
+  resolve: {
+    alias: {
+      // Helpful alias for importing assets
+      assets: path.resolve(__dirname, "src/assets"),
     },
-};
-
-module.exports = (env, argv) => {
-    if (argv.mode === 'development') {
-        console.log('!!! devServer devServer devServer !!!');
-        config.devServer = {
-            port: 58080,
-            writeToDisk: true
-        };
-    } else {
-        config.plugins.push(new CleanWebpackPlugin() );
-    }
-    return config;
+  },
 };
